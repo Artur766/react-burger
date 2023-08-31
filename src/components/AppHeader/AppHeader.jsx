dd253b1085633c22f34c9fdb6b93b17cd6d4446e
Adgamov Artur <adgamoffartur@yandex.ru>
2023-08-31 13:43:41 +0300

    Добавлено: компонент бургер меню

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "./AppHeader.module.css";
 import { Logo, BurgerIcon, ListIcon, ProfileIcon, MenuIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import logo from "../../images/logo.svg"
+import BurgerMenu from './BurgerMenu/BurgerMenu';
 function AppHeader() {
   return (
     <header className={styles.header}>
@@ -10,6 +11,7 @@ function AppHeader() {
           <a href=""><img src={logo} alt="" /></a>
           <MenuIcon />
         </div>
+        <BurgerMenu />
         <div className={styles.container}>
           <nav className={styles.navigation}>
             <a href="#" className={`${styles.navigationItem} ${styles.activeNavigation}`}>
---
+++ src/components/AppHeader/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from "./BurgerMenu.module.css";
+import { CloseIcon, ProfileIcon, BurgerIcon, ListIcon, ArrowDownIcon, ArrowUpIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+function BurgerMenu({ closeMenu }) {
+  const [isProfile, setIsProfile] = React.useState(false);
+  function handleMenuProfile() {
+    setIsProfile(!isProfile)
+  }
+  return (
+    <div className={styles.burger}>
+      <div className={styles.list}>
+        <div className={styles.header}>
+          <h2 className={styles.title}>Меню</h2>
+          <CloseIcon type="primary" onClick={closeMenu} />
+        </div>
+        <nav className={styles.navigation}>
+          <div className={styles.profileContainer} onClick={handleMenuProfile}>
+            <a href="#" className={styles.navigationItem}> <ProfileIcon type="primary" />Личный кабинет</a>
+            {isProfile ? <ArrowUpIcon type="primary" /> : <ArrowDownIcon type="primary" />}
+          </div>
+          {isProfile &&
+            <>
+              <a href="#" className={styles.linkProfile}>Профиль</a>
+              <a href="#" className={styles.linkProfile}>История заказов</a>
+              <a href="#" className={styles.linkProfile}>Выход</a>
+            </>
+          }
+          <a href="#" className={`${styles.navigationItem}`} onClick={closeMenu}>
+            <BurgerIcon type="secondary" />Конструктор бургеров
+          </a>
+          <a href="#" className={styles.navigationItem} onClick={closeMenu}> <ListIcon type="secondary" />Лента заказов</a>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default BurgerMenu;
